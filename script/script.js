3ca59c9a4abe8bea6332c2f8f0a5db99c8b759be
arthurbe08 <arthurbe08@gmail.com>
2026-05-09 20:01:12 -0300

    adicionando movimentação na home :D

--- script/script.js
+++ script/script.js
@@ -1,6 +1,14 @@
 import * as THREE from 'https://esm.sh/three@0.180.0';
-
+import { gsap } from 'https://cdn.jsdelivr.net/npm/gsap@3.12.5/index.js';
 import { GLTFLoader } from 'https://esm.sh/three@0.180.0/examples/jsm/loaders/GLTFLoader.js';
+
+//para a main
+let radius = 500;
+let targetRadius = 500;
+
+let theta = 0;
+let phi = Math.PI / 3;
+//para a main
 
 const scene = new THREE.Scene();
 const canvasScene = document.getElementById("scene");
@@ -29,10 +37,9 @@ camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvasScene,
-  antialias: true
+  antialias: true,
+  alpha: true
 });
-
-renderer.setSize(window.innerWidth, window.innerHeight);
 
 const loader = new GLTFLoader();
 
@@ -95,14 +102,63 @@ loader.load(
 function animate() {
 
   requestAnimationFrame(animate);
+  //dentro do animate
+  camera.position.x = radius/3 * Math.sin(phi) * Math.cos(theta);
 
-  // gira cada modelo
-  models.forEach(model => {
-    model.rotation.y += 0.01;
-  });
+  camera.position.y = radius/3 * Math.cos(phi);
+
+  camera.position.z = radius/3 * Math.sin(phi) * Math.sin(theta);
+
+  radius += (targetRadius - radius) * 0.1;
+
+  camera.lookAt(0, 20, 0);
 
   renderer.render(scene, camera);
+  //dentro do animate
 
 }
 
 animate();
+//movimentação da camera
+//camera movement//
+let mouseDown = false;
+
+let mouseX = 0;
+let mouseY = 0;
+
+
+document.addEventListener('mousedown', () => {
+
+    mouseDown = true;
+
+});
+
+document.addEventListener('mouseup', () => {
+
+    mouseDown = false;
+
+});
+
+document.addEventListener('mousemove', (event) => {
+
+    if (!mouseDown) return;
+
+    theta += event.movementX * 0.001;
+
+    phi -= event.movementY * 0.001;
+
+    
+    // LIMIT TO NORTH HEMISPHERE
+
+    phi = Math.max(0.1, Math.min(Math.PI / 2, phi));
+
+});
+
+document.addEventListener('wheel', (event) => {
+
+    targetRadius += event.deltaY * 0.5;
+
+    targetRadius = Math.max(100, Math.min(1000, targetRadius));
+
+});
+//movimentação da camera
