f1219df52d5a5410149febf6c89c9672c7536f9f
Felipe Bianchi Stefens <lipebianchistefens@gmail.com>
2026-05-09 20:34:42 -0300

    Changing CSS style

--- script/home.js
+++ script/home.js
@@ -2,8 +2,7 @@ const music = document.getElementById("music");
 
 window.addEventListener("click", () => {
 
-  music.volume = 0.5;
-
-  music.play();
+    music.volume = 0.5;
+    music.play();
 
 }, { once: true });
